15a3f474fa98a2e8e40fef4537b15e06e03b3a3b
Mike Dillon <mike@appropriate.io>
2015-06-01 21:37:03 -0700

    Make Admin::DashboardController an admin controller

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,5 @@
 class Admin
-  class DashboardController < ApplicationController
-    layout 'admin'
-
+  class DashboardController < AdminController
     def index
       redirect_to new_session_path(:admin) unless admin_signed_in?
       @orgs = policy_scope(Organization) if current_admin
